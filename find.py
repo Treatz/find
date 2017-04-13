3f1a61d4e3d8b392fb76ad79fdec5a624b8bad16
Corey White <adventmagic@gmail.com>
2017-04-13 19:06:07 -0400

    search

--- find.py
+++ find.py
@@ -1,38 +1,53 @@
-from evennia.commands.default.muxcommand import MuxCommand
+from evennia import default_cmds
 
 
-class CmdFind(MuxCommand):
+class CmdFind(default_cmds.MuxCommand):
     key = "+locate"
     locks = "cmd:all()"
-    def func(self):
-        skip = 1
-        for ishere in self.caller.location.contents:
-            if self.args == ishere.key:
-                self.msg("It is right here!\n")
-                skip = 0
 
-        if skip:
-            for arg in self.caller.location.exits:
-                for matches in arg.destination.contents:
-                    if self.args == matches.key:
-                        self.msg("(1)You sense it is %s." % arg)
-                        return
-                    else:
-                        for arg2 in matches.location.exits:
-                           for matches2 in arg2.destination.contents:
-                               if self.args == matches2.key:
-                                   self.msg("(2)You sense it is %s." % arg)	                  
-                                   return
-                               else:
-                                   for arg3 in matches2.location.exits:
-                                       for matches3 in arg3.destination.contents:
-                                           if self.args == matches3.key:
-                                               self.msg("(3)You sense it is %s." % arg)	                  
-                                               return
-                                           else:
-                                               for arg4 in matches3.location.exits:
-                                                   for matches4 in arg4.destination.contents:
-                                                       if self.args == matches4.key:
-                                                           self.msg("(4)You sense it is %s." % arg)	                  
-                                                           return
-                                                   
+	maxdepth = 4
+
+    def func(self):
+		"""confirms the target and initiates the search"""
+		
+		# save the target object onto the command
+		# this will use Evennia's default multimatch handling if more than one object matches
+		self.target = self.caller.search(self.args, global_search=True)
+
+		# initialize a list to store rooms we've visited
+		self.visited = []
+
+		# now start the search, passing in depth=0 
+		if not self._searcher(self.caller.location, 0):
+			# give the 'not found' message
+			self.caller.msg("You are unable to determine which way to go.")
+
+	def _searcher(self, room, depth):
+		"""Searches surrounding rooms recursively for an object"""
+
+		# first, record that we've been here
+		self.visited.append(room)
+
+		# our end condition is either when the item is found...
+		if self.target in room.contents:
+			if depth == 0:
+				self.caller.msg("It is right here!")
+			else:
+				self.caller.msg("({})You sense it is {}".format(depth, self.direction)
+			return True
+		
+		# or we have traveled `maxdepth` rooms away
+		if depth > self.maxdepth:
+			return False
+
+		# it's not in the current room, so loop through the exits and check them,
+		# skipping rooms we've already visited
+		exits = [exit for exit in room.exits if exit.destination not in self.visited]
+		for next in exits:
+			if depth == 0:  # we only want to return the exit out of the current room
+				self.direction = exit.key
+			if self._searcher(next.destination, depth+1):  # if we found the object, stop searching
+				return True
+
+		# we've checked all the exits, so return false
+		return False
